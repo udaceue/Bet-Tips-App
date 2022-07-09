d0b8188e0fefbc55f1191040dba0ad6dd8672ac4
Udaceue <rjemik3@gmail.com>
2022-07-09 23:51:43 +0200

    Sure Bet styles + final logic

--- src/components/sureBet/SureBetCalc.jsx
+++ src/components/sureBet/SureBetCalc.jsx
@@ -48,22 +48,30 @@ function SureBetCalc() {
   const checkSureBetHome = () => {
     const firstCheck = 1 / (1 / firstRay + 1 / secondRay);
     const stakeForHome = (stake * firstCheck) / firstRay;
-    console.log(`${stakeForHome} stake for Home`);
+    const fixedStakeForHome = stakeForHome.toFixed(2);
+    console.log(`${fixedStakeForHome} stake for Home`);
 
-    return stakeForHome;
+    return fixedStakeForHome;
   };
 
   const checkSureBetAway = () => {
     const firstCheck = 1 / (1 / firstRay + 1 / secondRay);
     const stakeForAway = (stake * firstCheck) / secondRay;
-    console.log(`${stakeForAway} stake for Away`);
-    return stakeForAway;
+    const fixedStakeForAway = stakeForAway.toFixed(2);
+    console.log(`${fixedStakeForAway} stake for Away`);
+    return fixedStakeForAway;
   };
 
   const FinalHomeStake = () => {
     const lastHomeStake = (stake * checkSureBetHome()) / 100;
     console.log(`${lastHomeStake} is equal to`);
     return lastHomeStake;
+  };
+
+  const GuarantedWin = () => {
+    const win = FinalHomeStake() * firstRay;
+    const fixedWin = win.toFixed(2);
+    return fixedWin;
   };
 
   const exam = () => {
@@ -78,45 +86,56 @@ function SureBetCalc() {
 
   return (
     <div className="ui container">
-      <div className="ui equal width grid">
+      <div className="ui equal width color grid">
         <div className="column">
           <div className="ui huge header textCenter">Sure Bet</div>
           <p className="textCenter">Check if sure bet is possible</p>
         </div>
         <div className="equal width row">
           <div className="column">
-            <input
-              type="number"
-              placeholder="Home Team"
-              step="0.1"
-              className="fullwidth"
-              onChange={handleHomeValue}
-            />
-          </div>
-          <i className="plus icon" />
-          <div className="column">
-            <input
-              type="number"
-              placeholder="Away Team"
-              step="0.1"
-              className="fullwidth"
-              onChange={handleAwayValue}
-            />
+            <label htmlFor="HomeTeamSure">
+              <h4>Home Team Odds</h4>
+              <input
+                id="HomeTeamSure"
+                type="number"
+                placeholder="Home Team"
+                step="0.1"
+                className="fullwidth"
+                onChange={handleHomeValue}
+              />
+            </label>
           </div>
           <div className="column">
-            <input
-              type="number"
-              placeholder="Stake for bets"
-              step="0.1"
-              className="fullwidth"
-              onChange={handleStake}
-              value={stake}
-            />
+            <label htmlFor="AwayTeamSure">
+              <h4>Away Team Odds</h4>
+              <input
+                id="AwayTeamSure"
+                type="number"
+                placeholder="Away Team"
+                step="0.1"
+                className="fullwidth"
+                onChange={handleAwayValue}
+              />
+            </label>
+          </div>
+          <div className="column">
+            <label htmlFor="SureStake">
+              <h4>Your Stake</h4>
+              <input
+                id="SureStake"
+                type="number"
+                placeholder="Stake for bets"
+                step="0.1"
+                className="fullwidth"
+                onChange={handleStake}
+                value={stake}
+              />
+            </label>
           </div>
         </div>
         <div className="column">
           <label htmlFor="FinalRake">
-            Rake in percentages%
+            <p>Profit in percentages</p>
             <input
               id="FinalRake"
               type="number"
@@ -127,7 +146,7 @@ function SureBetCalc() {
             />
           </label>
 
-          <div className="column">
+          <div className="column fullwidth">
             <button
               onClick={() => {
                 exam();
@@ -137,8 +156,48 @@ function SureBetCalc() {
               Check
             </button>
           </div>
-          <h3>{checkSureBetHome()}</h3>
-          <h3>{checkSureBetAway()}</h3>
+
+          <div className="ui two column grid">
+            <div className="left floated column textCenter">
+              <h3>You need to bet</h3>
+              <input
+                type="number"
+                placeholder="Stake For Home"
+                step="0.1"
+                className="fullwidth textCenter"
+                value={checkSureBetHome()}
+              />
+              <h4>On Home</h4>
+            </div>
+
+            <div className="right floated column textCenter segmentSpace">
+              <h3>You need to bet</h3>
+              <input
+                type="number"
+                placeholder="Stake for Away"
+                step="0.1"
+                className="fullwidth textCenter"
+                value={checkSureBetAway()}
+              />
+              <h4>On Away</h4>
+            </div>
+          </div>
+
+          <div className="column">
+            <label htmlFor="SureProfit">
+              <h2 className="greenText textCenter">
+                You will win in both cases
+              </h2>
+              <input
+                id="SureProfit"
+                type="number"
+                placeholder="Guaranted win  "
+                step="0.1"
+                className="fullwidth textCenter"
+                value={GuarantedWin()}
+              />
+            </label>
+          </div>
         </div>
       </div>
     </div>
